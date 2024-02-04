da84cca33656d775231fb7ae1f6b15c763c94448
Alvaro <alvarorent2001@gmail.com>
2024-02-04 04:54:17 -0500

    update

--- src/Pages/Carrito.jsx
+++ src/Pages/Carrito.jsx
@@ -45,32 +45,53 @@ function Carrito() {
 
   return (
     <>
-      <div className="grid grid-cols-3 py-10">
-        <div className="col-span-2">
-          <h1 className="text-4xl">Carrito de compras</h1>
+      <div className="pt-5 border-b border-slate-300">
+        <h3 className="text-2xl font-medium border-b-2 border-orange-500 inline-block pb-2">
+          Carrito de Compras
+        </h3>
+      </div>
+      <div className="flex flex-col sm:flex-row py-4 justify-center">
+        <div className="w-full sm:w-2/3">
           <div className="py-3 flex flex-col gap-3">
             {uniqueCars.map((cars, index) => (
-              <div key={index} className="flex gap-3">
+              <div
+                key={index}
+                className="flex flex-col items-center sm:flex-row gap-3"
+              >
                 <div>
                   <img className="w-40" src={cars.images[0]} alt={cars.title} />
                 </div>
                 <div>
                   <div>
-                    <h1>{cars.title}</h1>
-                    <p>S/.{cars.price}</p>
+                    <h1 className="text-base text-blue-600 font-bold">
+                      {cars.title}
+                    </h1>
+                    <p className="text-lg text-gray-700 font-medium">
+                      S/.{cars.price}
+                    </p>
                   </div>
                   <div>
-                    <h1>Cantidad:</h1>
-                    <button onClick={() => resClick(index)}><RiSubtractFill/></button>
-                    <input type="text" value={quantities[index]} disabled />
-                    <button onClick={() => sumClick(index)}><IoMdAdd/></button>
+                    <h1 className="text-lg text-gray-700 font-medium">
+                      Cantidad:
+                    </h1>
+                    <div className="flex">
+                      <button onClick={() => resClick(index)}>
+                        <RiSubtractFill />
+                      </button>
+                      <input type="text" value={quantities[index]} disabled />
+                      <button onClick={() => sumClick(index)}>
+                        <IoMdAdd />
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="col-span-1">RESUMEN DE TU PEDIDO</div>
+        <div>
+          <h3>RESUMEN DE TU PEDIDO</h3>
+        </div>
       </div>
       <Catgorys />
     </>
