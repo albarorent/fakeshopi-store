c85e54dbbd74f785e047ff5f6be01315b3bf020e
Alvaro <alvarorent2001@gmail.com>
2024-02-06 13:51:32 -0500

    update

--- src/Components/LazyImage.jsx
+++ src/Components/LazyImage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 const LazyImage = ({ src, alt, w = "w-52  h-52" }) => (
   <picture>
-    <source srcSet={src} type="image/webp" />
-    <source srcSet={src} type="image/jpg" />
+    <source srcSet={isValidUrl(src) ? src : ""} type="image/webp" />
+    <source srcSet={isValidUrl(src) ? src : ""} type="image/jpg" />
     <img
       className={`${w}`}
       src={src}
