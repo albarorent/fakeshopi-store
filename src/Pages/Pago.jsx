6c9681803c269096bcf3c2750b599b565bf16944
Alvaro <alvarorent2001@gmail.com>
2024-02-09 12:50:08 -0500

    update

--- src/Pages/Pago.jsx
+++ src/Pages/Pago.jsx
@@ -1,27 +1,166 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useShopify } from "../context/ShopifyContext";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import SweetAlert from "../Components/SweetAlert";
 
 function Pago() {
-  const { idCount, subtotal } = useShopify();
-  
+  const { idCount, subtotal, setcantCar, setCar } = useShopify();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    if (!idCount || !subtotal) {
+      navigate("/carrito");
+    }
+  }, []);
+
+  const onSubmit = handleSubmit((data) => {
+    SweetAlert(`Gracias por su compra ${data.first_name}. Vuelva pronto.`);
+    localStorage.removeItem("car");
+    const storedCar = JSON.parse(localStorage.getItem("car")) || [];
+    setcantCar(0);
+    setCar(storedCar);
+    navigate("/");
+  });
 
   return (
-    <div>
+    <div className="w-full">
       <div className="pt-5 border-b border-slate-300">
         <h3 className="text-2xl font-medium border-b-2 border-orange-500 inline-block pb-2">
           Datos del Cliente
         </h3>
       </div>
+      <div className="flex flex-col sm:flex-row gap-3 py-5">
+        <div className="w-full sm:w-[800px] ">
+          <form onSubmit={onSubmit}>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="first_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Nombres
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John"
+                  required
+                  {...register("first_name", { required: true })}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="last_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Apellidos
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Doe"
+                  required
+                  {...register("last_name", { required: true })}
+                />
+              </div>
 
-      <form action="#" className="flex flex-col">
-        <label htmlFor="name">Nombres</label>
-        <input id="name" type="text" />
-        <label htmlFor="lastname">Apellidos</label>
-        <input id="lastname" type="text" />
-        <label htmlFor="address">Dirección</label>
-        <input id="address" type="text" />
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Telefono
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="123-458-678"
+                  required
+                  {...register("telephone", { required: true })}
+                />
+              </div>
 
-      </form>
+              <div>
+                <label
+                  htmlFor="address"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Dirección
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="address"
+                  required
+                  {...register("address", { required: true })}
+                />
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john.doe@company.com"
+                required
+                {...register("email", { required: true })}
+              />
+            </div>
+
+            <div className="flex items-start mb-6">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <label
+                htmlFor="remember"
+                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Aceptar{" "}
+                <a
+                  href="#"
+                  className="text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  terminos y condiciones
+                </a>
+                .
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        <div>
+          <h1 className="text-xl font-medium">PRECIO TOTAL</h1>
+          <p className="text-xl font-medium">S/.{subtotal + 100}</p>
+        </div>
+      </div>
     </div>
   );
 }
